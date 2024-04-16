9487f63d3bbf176448bfe1e3ce7c1d4bcef3c4e6
Melnichenko Evgenii <Sovenokk25@gmail.com>
2024-04-16 19:21:35 +0300

    commit

---
+++ shop/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name', )
+    list_editable = ('description', )
+    ordering = ('name', )
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name', )
+    ordering = ('name', )
+
+@admin.register(Pos_parameter)
+class Pos_parameterAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product', 'parameter', 'value')
+    list_display_links = ('pk', )
+    search_fields = ('product', 'parameter')
+    list_editable = ('value', )
+    list_filter = ('product', 'parameter')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'category', 'price', 'exists', 'date_create')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name', 'price')
+    list_editable = ('price', 'exists')
+    list_filter = ('category', 'exists')
+    ordering = ('date_create', 'name')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'customer', 'price', 'date_create')
+    list_display_links = ('pk', )
+    search_fields = ('customer', )
+    list_editable = ('customer', 'price')
+    ordering = ('date_create', 'date_finish')
+
+@admin.register(Pos_order)
+class Pos_orderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product', 'order', 'count')
+    list_display_links = ('pk', )
+    search_fields = ('product', 'order')
+    list_filter = ('product', 'order')
+    list_editable = ('count', )
---
+++ shop/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+
+MAX_LENGTH_CHAR = 255
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH_CHAR, unique=True, verbose_name='Название')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Parameter(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH_CHAR, unique=True, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Характеристика'
+        verbose_name_plural = 'Характеристики'
+
+class Pos_parameter(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name='Товар')
+    parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE, verbose_name='Характеристика')
+    value = models.CharField(max_length=MAX_LENGTH_CHAR, verbose_name='Значение характеристики')
+
+    def __str__(self):
+        return f'{self.pk} {self.product.name} {self.parameter.name} {self.value}'
+
+    class Meta:
+        verbose_name = 'Позиция характеристики'
+        verbose_name_plural = 'Позиции характеристик'
+
+class Product(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH_CHAR, verbose_name='Название')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.FloatField(verbose_name='Стоимость')
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_update = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+    photo = models.ImageField(upload_to='image/%Y/%m/%d', null=True, blank=True, verbose_name='Фотография')
+    exists = models.BooleanField(default=True, verbose_name='Существует')
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    parameters = models.ManyToManyField(Parameter, through=Pos_parameter, verbose_name='Параметры')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Заказчик')
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_finish = models.DateTimeField(null=True, blank=True, verbose_name='Дата завершения')
+    price = models.FloatField(default=0.0, verbose_name='Стоимость')
+
+    products = models.ManyToManyField(Product, through='Pos_order', verbose_name='Товары')
+
+    def __str__(self):
+        return f'{self.pk} {self.customer} {self.date_create}'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+class Pos_order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар')
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заказ')
+    count = models.PositiveIntegerField(default=1, verbose_name='Количество товара')
+
+    def __str__(self):
+        return f'{self.pk} {self.product.name} {self.order.customer} ({self.count} шт.)'
+
+    def sum_pos_order(self):
+        return self.product.price * self.count
+
+    class Meta:
+        verbose_name = 'Позиция заказа'
+        verbose_name_plural = 'Позиции заказов'
