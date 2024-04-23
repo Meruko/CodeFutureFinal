b2cbc6f6bfd3da2f00e06b73cc174cf8ca147fc4
Melnichenko Evgenii <Sovenokk25@gmail.com>
2024-04-23 20:04:33 +0300

    23.04

---
+++ accounts/templatetags/account_form.py
@@ -0,0 +1,13 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('accounts/_inc/_form.html')
+def account_form(title: str, form, url_name: str):
+    return {
+        'title': title,
+        'form': form,
+        'url_name': url_name
+    }
---
+++ shop/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'description')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name', )
+    list_editable = ('description', )
+    ordering = ('name', )
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'customer', 'date_create')
+    list_display_links = ('pk', )
+    search_fields = ('customer', )
+    list_editable = ('customer', )
+    list_filter = ('customer', )
+    ordering = ('date_create', 'date_finish')
+    
+@admin.register(Pos_order)
+class Pos_orderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product', 'order', 'count')
+    list_display_links = ('pk', )
+    search_fields = ('product', 'order', 'count')
+    list_editable = ('count', )
+    list_filter = ('product', 'order')
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name', )
+    ordering = ('name', )
+
+@admin.register(Pos_parameter)
+class Pos_parameterAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'product', 'parameter', 'value')
+    list_display_links = ('pk', )
+    search_fields = ('product', 'parameter')
+    list_editable = ('value', )
+    list_filter = ('product', 'parameter')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'price', 'exists')
+    list_display_links = ('pk', 'name')
+    search_fields = ('name', 'price')
+    list_editable = ('exists', )
+    ordering = ('date_create', 'name')
