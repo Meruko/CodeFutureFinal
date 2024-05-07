d799f4c892b99705925bd9e02b8f00bfaab189f7
Melnichenko Evgenii <Sovenokk25@gmail.com>
2024-05-07 21:14:18 +0300

    commit

--- basket/urls.py
+++ basket/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import *
+from . import views
 
 urlpatterns = [
-
+    path('', views.basket_detail, name='basket'),
+    path('add/<int:product_id>/', views.basket_add, name='basket_add'),
+    path('remove/<int:product_id>/', views.basket_remove, name='basket_remove'),
+    path('clear/', views.basket_clear, name='basket_clear'),
+    path('buy/', views.basket_buy, name='basket_buy'),
 ]
